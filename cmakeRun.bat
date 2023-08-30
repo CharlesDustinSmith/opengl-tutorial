a621d378b234c7b65d094463d06242bbb3765be5
Charles Dustin Smith <charlesdsmith19@gmail.com>
2023-08-30 12:00:18 -0230

    Fixed the bugs with the cmake files, now working on fixing the bugs with the source code, getting GLEW initialisation failed! Window failed to open! :(

--- cmakeRun.bat
+++ cmakeRun.bat
@@ -1,14 +1,15 @@
 @REM Run cmake command to configure the project.
 cmake -S . ^
--B ./Build
+-B ./build ^
 -DCOMPILE_EXECUTABLE=ON ^
 -DUSE_VCPKG=ON ^
 -G "Visual Studio 17 2022" ^
 -A x64
 
 @REM Command to build the cmake project.
-cd Build
-MSBuild.exe ./OpenGL-Tutorial.sln
+cd build
+MSBuild.exe ./OpenGLTutorial.sln
+@REM cmake --build ./Build --config Debug
 
 @REM Running the executable
-
+./app/Debug/OpenGLTut.exe
